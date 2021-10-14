3d9be2e8c92b489f7a7ce37b3e8702d7132dfcac
Naokirlz <cristian_pd88@hotmail.com>
2021-10-13 21:17:22 -0300

    Create 11- opcional-chaining.ts

---
+++ ts-intro/src/ejercicios/11- opcional-chaining.ts
@@ -0,0 +1,28 @@
+
+/*
+    ===== Código de TypeScript =====
+*/
+
+interface Pasajero {
+    nombre:string;
+    hijos?: string[];
+}
+
+const pasajero1: Pasajero = {
+    nombre:'Cristian'
+}
+
+const pasajero2 : Pasajero = {
+    nombre :'Melisa',
+    hijos: ['natalia', 'gabriel']
+}
+
+
+function imprimeHijos (pasajero:Pasajero):void {
+
+    const cuantosHijos = pasajero.hijos?.length || 0;
+    console.log (cuantosHijos);
+
+}
+
+imprimeHijos (pasajero1);
