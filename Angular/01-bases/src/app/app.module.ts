9c400e7f7e41481d56c15f996940a70e0139ff33
Naokirlz <cristian_pd88@hotmail.com>
2021-10-19 22:14:32 -0300

    proyecto angular

---
+++ Angular/01-bases/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { HeroeModule } from './heroes/heroes.module';
+import { ContadorModule } from './contadores/contadores.module';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HeroeModule,
+    ContadorModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
