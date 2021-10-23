0fc5365abb00563b79cc8d531bf1572f893373e8
Naokirlz <cristian_pd88@hotmail.com>
2021-10-23 17:33:58 -0300

    fin seccion 7

---
+++ Angular/02-giftsApp/src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,77 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { SearchGifsResponse, Gif } from '../interface/gifs.interface';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+
+  private apiKey    : string = 'ZxGkaQ3Qt9z5BV80EDdTW0A1zAjgdMn8';
+  private servicioUrl:string = 'https://api.giphy.com/v1/gifs';
+  private _historial: string[] = [];
+
+  public resultados: Gif[]= [];
+
+  get historial(){
+    
+    return [...this._historial];
+
+  }
+
+  constructor(private http: HttpClient){
+
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!) || [];
+
+    // resultados
+   
+  //   if(localStorage.getItem('historial')){
+  //   this._historial = JSON.parse(localStorage.getItem('historial')! );
+  //  }
+
+  }
+
+  //async 
+  buscarGifs( query: string){
+
+      query = query.trim().toLowerCase();
+
+      if(query.trim().length === 0){return;}
+
+      if(!this._historial.includes(query)){
+        
+        this._historial = this._historial.splice(0,10);
+        this._historial.unshift(query);
+
+       localStorage.setItem('historial', JSON.stringify(this._historial)) ;
+
+        console.log(this._historial);
+
+      }
+
+      const params = new HttpParams()
+        .set('api_key', this.apiKey)
+        .set('limit', '10')
+        .set('q', query);
+
+
+      this.http.get<SearchGifsResponse>(`${this.servicioUrl}/search`,{params} )
+        .subscribe( (resp) => {
+          console.log(resp.data);
+          this.resultados = resp.data;
+          localStorage.setItem('resultados', JSON.stringify(this.resultados)) ;
+
+        } )
+
+
+      // const resp = await fetch('https://api.giphy.com/v1/gifs/search?api_key=ZxGkaQ3Qt9z5BV80EDdTW0A1zAjgdMn8&q=dagon ball z&limit=10');
+      // const data = await resp.json();
+      // console.log(data);
+
+    }
+
+            
+
+  }
