92def53b4918e43bf8d8f0575c0639e05794b31b
Naokirlz <cristian_pd88@hotmail.com>
2021-10-23 12:45:56 -0300

    despliegue
    https://cocky-ride-7169af.netlify.app/

--- Angular/01-bases/src/app/dbz/main-page/main-page.component.ts
+++ Angular/01-bases/src/app/dbz/main-page/main-page.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Personaje } from '../interfaces/dbz.interface';
-import { AgregarComponent } from '../agregar/agregar.component';
 import { DbzService } from '../services/dbz.service';
 
 
